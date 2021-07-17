58079ba5733d9560c971323fd3fd99f7da1b2f8e
pocketContributor <842100455@qq.com>
2021-07-17 14:51:05 +0800

    hystrix -> dashboard configuration

--- cloud-consumer-hystrix-dashboard9001/src/main/java/com/nasuf/springcloud/HystrixDashboardMain9001.java
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/nasuf/springcloud/HystrixDashboardMain9001.java
@@ -1,10 +1,15 @@
 package com.nasuf.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableHystrixDashboard
 public class HystrixDashboardMain9001 {
     public static void main(String[] args) {
