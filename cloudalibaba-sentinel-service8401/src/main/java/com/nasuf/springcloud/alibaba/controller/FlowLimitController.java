556bf1385a8e6a4b7ecda1d8da5d28715442feae
pocketContributor <842100455@qq.com>
2021-07-25 14:10:54 +0800

    sentinel -> @SentinelResource with blockHandler for hotKey

--- cloudalibaba-sentinel-service8401/src/main/java/com/nasuf/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/nasuf/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,6 +1,9 @@
 package com.nasuf.springcloud.alibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,12 +11,28 @@ public class FlowLimitController {
 
     @GetMapping("/testA")
     public String testA() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return "### testA.";
     }
 
     @GetMapping("/testB")
     public String testB() {
         return "### testB.";
+    }
+
+    @GetMapping("/testHotkey")
+    @SentinelResource(value = "testHotkey", blockHandler = "deal_testHotkey") // value可以任意，唯一即可
+    public String testHotkey(@RequestParam(value = "p1", required = false) String p1,
+                             @RequestParam(value = "p2", required = false) String p2) {
+        return "### testHotkey";
+    }
+
+    public String deal_testHotkey(String p1, String p2, BlockException exception) {
+        return "### deal_testHotkey";
     }
 
 }
